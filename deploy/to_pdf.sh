0a270fe7c0d6a5f8ea75057971e696164522fb5c
Nick Chimicles <nac2197@columbia.edu>
2025-04-06 20:38:29 -0400

    double space.

--- deploy/to_pdf.sh
+++ deploy/to_pdf.sh
@@ -1,52 +1,56 @@
 #!/bin/bash
 
-echo "Combining markdown files..."
-
-# Create a combined markdown file from revised files
-cat acknowledgements.md > combined_thesis.md
-
-cat buffer.md >> combined_thesis.md
-
-cat preface.md >> combined_thesis.md
-
-cat buffer.md >> combined_thesis.md
-
-cat introduction.md >> combined_thesis.md
-
-cat buffer.md >> combined_thesis.md
-
-cat literature_review.md >> combined_thesis.md
-
-cat buffer.md >> combined_thesis.md
-
-cat chapter_1.md >> combined_thesis.md
-
-cat buffer.md >> combined_thesis.md
-
-cat chapter_2.md >> combined_thesis.md
-
-cat buffer.md >> combined_thesis.md
-
-cat conclusion.md >> combined_thesis.md
-
-cat buffer.md >> combined_thesis.md
-
-cat bibliography.md >> combined_thesis.md
-
-cat buffer.md >> combined_thesis.md
-
-cat methodology_attack_data.md >> combined_thesis.md
-
-cat buffer.md >> combined_thesis.md
-
-cat results_attack_data.md >> combined_thesis.md
+TITLE="The Persistence of Precision: Challenging the Narrative of American Strategic Bombing in World War II" 
 
 
-echo "Updating links..."
-python deploy/update_links.py combined_thesis.md
 
-echo "Updating footnotes..."
-python renumber_footnotes.py combined_thesis.md
+# echo "Combining markdown files..."
+
+# # Create a combined markdown file from revised files
+# cat acknowledgements.md > combined_thesis.md
+
+# cat buffer.md >> combined_thesis.md
+
+# cat preface.md >> combined_thesis.md
+
+# cat buffer.md >> combined_thesis.md
+
+# cat introduction.md >> combined_thesis.md
+
+# cat buffer.md >> combined_thesis.md
+
+# cat literature_review.md >> combined_thesis.md
+
+# cat buffer.md >> combined_thesis.md
+
+# cat chapter_1.md >> combined_thesis.md
+
+# cat buffer.md >> combined_thesis.md
+
+# cat chapter_2.md >> combined_thesis.md
+
+# cat buffer.md >> combined_thesis.md
+
+# cat conclusion.md >> combined_thesis.md
+
+# cat buffer.md >> combined_thesis.md
+
+# cat bibliography.md >> combined_thesis.md
+
+# cat buffer.md >> combined_thesis.md
+
+# cat methodology_attack_data.md >> combined_thesis.md
+
+# cat buffer.md >> combined_thesis.md
+
+# cat results_attack_data.md >> combined_thesis.md
+
+
+# echo "Updating links..."
+# python deploy/update_links.py combined_thesis.md
+
+# echo "Updating footnotes..."
+# python renumber_footnotes.py combined_thesis.md
 
 echo "Converting to PDF..."
 pandoc combined_thesis.md \
@@ -55,7 +59,7 @@ pandoc combined_thesis.md \
     --template=deploy/template.tex \
     --toc \
     --number-sections \
-    -V title="American Exceptionalism in War: Precision Bombing and Resisting the Temptation of Total War" \
+    -V title="The Persistence of Precision: Challenging the Narrative of American Strategic Bombing in World War II" \
     -V author="Nicholas A. Chimicles" \
     -V date="\today" \
     -V geometry:margin=1in \
